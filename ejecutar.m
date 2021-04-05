5c377b17d591d690bf018d8dead4c0810ed0561e
z_tjalezea <z_tjalezea@yahoo.com>
2021-04-05 10:38:28 -0500

    Removing temp and cached files

--- ejecutar.m
+++ ejecutar.m
@@ -1,3 +1,21 @@
+%
+
+%{
+Laboratorio de Inteligencia y Visión Artificial
+ESCUELA POLITÉCNICA NACIONAL
+Quito - Ecuador
+
+autor: ztjona!
+jonathan.a.zea@ieee.org
+
+"I find that I don't understand things unless I try to program them."
+-Donald E. Knuth
+
+05 April 2021
+Matlab 9.9.0.1592791 (R2020b) Update 5.
+%}
+
+%% 
 %% Clean environment
 close all;
 % myoObject global variable that handles myo armband
