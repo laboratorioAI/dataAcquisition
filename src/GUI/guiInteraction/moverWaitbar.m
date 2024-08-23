4c8e1ee0349c84452ffabf963b5b83106226abfe
XLex0 <165972924+XLex0@users.noreply.github.com>
2024-08-23 13:37:15 -0500

    ADDED: Added sound before GO
    Sound beep has been added

--- src/GUI/guiInteraction/moverWaitbar.m
+++ src/GUI/guiInteraction/moverWaitbar.m
@@ -30,6 +30,30 @@ tamText = 12;
 colorLetter = [0 0 1];
 handles.msjText.String = '';
 
+
+if porcentaje < transicion / 3
+    handles.msjText.String = '3';
+    
+elseif (porcentaje >= transicion / 3) && (porcentaje < transicion * 2 / 3)
+    handles.msjText.String = '2';
+    
+elseif (porcentaje >= transicion * 2 / 3) && (porcentaje < transicion * 7/8)
+    handles.msjText.String = '1';
+
+elseif (porcentaje >= transicion * 7/8 ) && (porcentaje < transicion)
+    frecuencia = 3192;
+    time = 0:1/frecuencia:0.5;
+    y = sin(2*pi*1000*time);
+    sound(y, frecuencia);
+    
+elseif porcentaje >= transicion
+    handles.msjText.String = 'GO!';
+    colorLetter = [1 0 0];
+    handles.msjText.FontWeight = 'normal';
+    tamText = 18;
+end
+
+%{
 if porcentaje < transicion / 3
     handles.msjText.String = '3';
     
@@ -45,6 +69,7 @@ elseif porcentaje > transicion
     handles.msjText.FontWeight = 'normal';
     tamText = 18;
 end
+%}
 % if porcentaje*0.98 <= transicion && transicion <= 1.02*porcentaje
 %     beep
 % end
